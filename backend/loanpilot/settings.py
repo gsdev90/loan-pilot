90f1ba1dc459b799b14dcd18eb743cd1b18c4bb7
gsdev90 <gsdev90@gmail.com>
2025-05-14 22:11:14 +1000

    docker containerization

--- backend/loanpilot/settings.py
+++ backend/loanpilot/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -95,6 +95,17 @@ DATABASES = {
     }
 }
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME', 'loanpilot'),
+#         'USER': os.environ.get('DB_USER', 'myuser'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD', 'mypass'),
+#         'HOST': os.environ.get('DB_HOST', 'db'),
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
